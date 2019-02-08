92b9d7a40106fe7c5d7c5ad0115e5793d8d0fdb4
Akn <sibata486@gmail.com>
2019-02-08 23:05:50 +0900

    [機能追加]　ファイル読み込みのためのパーミッションリクエスト関数を追加しました。

--- app/src/main/java/com/example/main/android_image_viewer/FileSelectActivity.java
+++ app/src/main/java/com/example/main/android_image_viewer/FileSelectActivity.java
@@ -1,5 +1,9 @@
 package com.example.main.android_image_viewer;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import timber.log.Timber;
@@ -11,5 +15,13 @@ public class FileSelectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_select);
+        permissionRequest();
+    private void permissionRequest(){
+        String[] PERMISSIONS_STORAGE = { Manifest.permission.READ_EXTERNAL_STORAGE };
+        final int REQUEST_EXTERNAL_STORAGE = 1;
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if(permission != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE , REQUEST_EXTERNAL_STORAGE);
+        }
     }
 }
