eaf5c03c564ef0683eb08d5502db0c45faf9a2c7
Akn <sibata486@gmail.com>
2019-02-08 23:38:48 +0900

    [機能追加]　ピクチャーフォルダのパスを取得する機能を追加

--- app/src/main/java/com/example/main/android_image_viewer/FileSelectActivity.java
+++ app/src/main/java/com/example/main/android_image_viewer/FileSelectActivity.java
@@ -2,10 +2,14 @@ package com.example.main.android_image_viewer;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
+
+import java.io.File;
 import timber.log.Timber;
 
 
@@ -15,7 +19,14 @@ public class FileSelectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_select);
+
         permissionRequest();
+
+        //ストレージ内のピクチャーフォルダのパスを取得
+        File picDirectory =  Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File[] files = picDirectory.listFiles();
+    }
+    
     private void permissionRequest(){
         String[] PERMISSIONS_STORAGE = { Manifest.permission.READ_EXTERNAL_STORAGE };
         final int REQUEST_EXTERNAL_STORAGE = 1;
