db566acf443064d4f626d93fb16c0e720ec38c24
Akn <sibata486@gmail.com>
2019-02-16 21:40:47 +0900

    [機能修正]　サムネイルの生成を非同期処理で行うよう変更
    初めてフォルダーを開く際、画像の量によってはサムネ生成で長時間アクティビティが停止する。そのため、初回のサムネイル生成は非同期処理で行う。

--- app/src/main/java/com/example/main/android_image_viewer/ThumbnailFactory.java
+++ app/src/main/java/com/example/main/android_image_viewer/ThumbnailFactory.java
@@ -17,37 +17,9 @@ import static java.lang.Boolean.valueOf;
 public final class ThumbnailFactory {
 
     public void resizeFileInDirectory( File targetDirectory ) {
-        File currentDirectory = new File( targetDirectory.getPath() );
-        File[] pictureFiles = currentDirectory.listFiles( new FileTypeFilter() );
-
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize = 4;
-        options.inPreferredConfig = Bitmap.Config.RGB_565;
-
-        ArrayList<BitmapAndName> bitmaps = new ArrayList<>();
-
-        for(File f:pictureFiles){
-            Bitmap bmp = BitmapFactory.decodeFile( f.getPath() , options );
-            BitmapAndName bitmapAndName = new BitmapAndName();
-            bitmapAndName.bitmap = bmp;
-            bitmapAndName.fileName = f.getName();
-            bitmaps.add( bitmapAndName );
-        }
-
-        Context context = AndroidImageViewer.getAppContext();
-
-        try {
-            for(BitmapAndName bmp : bitmaps){
-                File destination = new File( context.getFilesDir().getPath() + "/" + bmp.fileName);
-                FileOutputStream fileOutputStream = new FileOutputStream(destination);
-                bmp.bitmap.compress(Bitmap.CompressFormat.JPEG , 50 ,  fileOutputStream);
-                fileOutputStream.close();
-            }
-
-        }catch (Exception e){
-            Timber.i("Error" + e.toString());
-        }
-
+        File[] files = { targetDirectory };
+        AsyncBitmapDecoder task = new AsyncBitmapDecoder();
+        task.execute( files );
     }
 
     private class BitmapAndName{
@@ -59,6 +31,46 @@ public final class ThumbnailFactory {
         public boolean accept(File file , String name){
             if(name.endsWith(".jpg")){ return true; }
             return false;
+        }
+    }
+
+    private class AsyncBitmapDecoder extends android.os.AsyncTask< File , Void ,  Boolean>{
+
+        @Override
+        protected Boolean doInBackground(File... targetDirectory){
+
+            File currentDirectory = new File( targetDirectory[0].getPath() );
+            File[] pictureFiles = currentDirectory.listFiles( new FileTypeFilter() );
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 8;
+            options.inPreferredConfig = Bitmap.Config.RGB_565;
+
+            ArrayList<BitmapAndName> bitmaps = new ArrayList<>();
+
+            for(File f:pictureFiles){
+                Bitmap bmp = BitmapFactory.decodeFile( f.getPath() , options );
+                BitmapAndName bitmapAndName = new BitmapAndName();
+                bitmapAndName.bitmap = bmp;
+                bitmapAndName.fileName = f.getName();
+                bitmaps.add( bitmapAndName );
+            }
+
+            Context context = AndroidImageViewer.getAppContext();
+
+            try {
+                for(BitmapAndName bmp : bitmaps){
+                    File destination = new File( context.getFilesDir().getPath() + "/" + bmp.fileName);
+                    FileOutputStream fileOutputStream = new FileOutputStream(destination);
+                    bmp.bitmap.compress(Bitmap.CompressFormat.JPEG , 30 ,  fileOutputStream);
+                    fileOutputStream.close();
+                }
+
+            }catch (Exception e){
+                Timber.i("Error" + e.toString());
+            }
+
+            return true;
         }
     }
 }
